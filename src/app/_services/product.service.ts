53a97447c45bcc79d1daf90a20a0de161edd6837
Sandip Kumar Mondal <sandip_mondal85@yahoo.co.in>
2018-12-07 20:07:35 +0530

    Add list product

---
+++ src/app/_services/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+import { Product } from '../_models';
+
+@Injectable({ providedIn: 'root' })
+export class ProductService{
+  constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get<Product[]>(`${environment.apiUrl}/api/products`);
+  }
+
+  create(product: Product){
+    this.http.post(`${environment.apiUrl}/api/products}`, product);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${environment.apiUrl}/api/products/${id}`);
+  }
+}
--- src/app/list-product/list-product.component.ts
+++ src/app/list-product/list-product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../_services';
+import { Product } from '../_models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-product',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-product.component.scss']
 })
 export class ListProductComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
+  constructor(private productservice: ProductService, private router: Router) { }
 
   ngOnInit() {
+    this.productservice.getAll().subscribe(data => { this.products = data; });
   }
 
+  deleteProduct(product: Product): void {
+    this.productservice.delete(product.id)
+      .subscribe(data => {
+        this.products = this.products.filter(u => u !== product);
+    })
+  };
+
+  editProduct(product: Product): void {
+    localStorage.removeItem("editUserId");
+    localStorage.setItem("editUserId", product.id.toString());
+    this.router.navigate(['edit-user']);
+  };
+
+  addProduct(): void {
+    this.router.navigate(['add-user']);
+  };
 }
